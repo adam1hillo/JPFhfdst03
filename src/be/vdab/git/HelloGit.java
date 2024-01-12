d2c3d47f572f6dd4a17ae18a1bf9c2df3c4a6d52
Adam Hillo <adam1hillo@gmail.com>
2024-01-12 11:09:28 +0100

    Bonjur tout le monde

--- src/be/vdab/git/HelloGit.java
+++ src/be/vdab/git/HelloGit.java
@@ -2,6 +2,6 @@ package be.vdab.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Bonjur Git");
+        System.out.println("Bonjour tout le monde !");
     }
 }
